239b46d6a3ac50a7fbe3e00d7b9acf7bd564f93e
Anastasia Fokina <45092650+stasechka11@users.noreply.github.com>
2022-07-05 13:56:46 +0300

    added console input to Praktikum

--- src/main/java/Praktikum.java
+++ src/main/java/Praktikum.java
@@ -1,10 +1,15 @@
 import java.util.Scanner;
 
 public class Praktikum {
-    public static String fullName = "Имя Фамилия ";
+    public static String fullName;
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите имя и фамилию владельца." +
+                "- В строке должно быть не меньше 3 и не больше 19 символов" +
+                "- В строке есть только один пробел" +
+                "- Пробел стоит не в начале и не в конце строки");
+        fullName = scanner.nextLine();
         Account account = new Account(fullName);
         System.out.println(account.checkNameToEmboss());
     }
