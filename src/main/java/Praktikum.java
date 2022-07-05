398c467a1ae316458b9f9e1c2b6e5c3e7d063958
Anastasia Fokina <45092650+stasechka11@users.noreply.github.com>
2022-07-05 14:09:21 +0300

    added line break to output

--- src/main/java/Praktikum.java
+++ src/main/java/Praktikum.java
@@ -5,9 +5,9 @@ public class Praktikum {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Введите имя и фамилию владельца." +
-                "- В строке должно быть не меньше 3 и не больше 19 символов" +
-                "- В строке есть только один пробел" +
+        System.out.println("Введите имя и фамилию владельца.\n" +
+                "- В строке должно быть не меньше 3 и не больше 19 символов\n" +
+                "- В строке есть только один пробел\n" +
                 "- Пробел стоит не в начале и не в конце строки");
         fullName = scanner.nextLine();
         Account account = new Account(fullName);
